244b8f7c3753944dd7a3982f107a413f6dc89fe1
RebecaZaia <rebeca.zaia@estudante.ifms.edu.br>
2026-08-14 11:20:57 -0400

    new file:   .gitignore, README.md, eslint.config.js, index.html, package-lock.json, package.json, src/App.css 	new file:   src/App.jsx, src/Exercicio1.jsx, src/Exercicio2.jsx, src/Exercicio3.jsx, src/Exercicio4.jsx, src/Exercicio5.jsx, src/ListaUsuarios.jsx 	new file:   src/index.css, src/main.jsx, vite.config.js

---
+++ src/ListaUsuarios.jsx
@@ -0,0 +1,13 @@
+function ListaUsuarios({ usuarios }) {
+  return (
+    <ul>
+      {usuarios.map((usuario) => (
+        <li key={usuario.id}>
+          {usuario.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default ListaUsuarios;
